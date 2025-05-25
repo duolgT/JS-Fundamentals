2f22d348bcb29b3ca039b886d00a3c948a287c03
DuolDaniel <duolkuach100@gmail.com>
2025-05-25 16:41:36 +0300

    New version of integers

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,10 +1,7 @@
 const args = process.argv.slice(2);
-
 const firstArg = args[0];
 
 const number = parseInt(firstArg);
-
-console.log(number);
 
 if (isNaN(number)) {
   console.log("Not a number");
